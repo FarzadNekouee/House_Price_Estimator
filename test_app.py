11ebe80f6651eb6873fd0bdf1a634fc6a54694b6
FarzadNekouee <nekouei.farzad@gmail.com>
2025-06-09 13:52:54 +0330

    ci workflow added

--- test_app.py
+++ test_app.py
@@ -8,8 +8,6 @@ def client():
 
 def test_predict(client):
     response = client.post('/predict', json={"features": [0.00632, 18.0, 2.31, 0.0, 0.538, 6.575, 65.2, 4.09, 1.0, 296.0, 15.3, 396.9, 4.98]})
-    print("------------", response.get_json())
     assert response.status_code == 200
-
     assert "prediction" in response.get_json()
     
